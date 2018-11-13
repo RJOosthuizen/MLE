ef6d402141731618dffb587f583aa620fbf754ac
RJOosthuizen <33512291+RJOosthuizen@users.noreply.github.com>
2018-11-13 16:02:58 +0200

    added multiclass logistic regression
    added multiclass logistic regression with regularization

--- machine-learning-ex3-complete/ex3/lrCostFunction.m
+++ machine-learning-ex3-complete/ex3/lrCostFunction.m
@@ -37,12 +37,13 @@ grad = zeros(size(theta));
 %
 
 
+J = (-1 / m) * sum(y.*log(sigmoid(X * theta)) + (1 - y).*log(1 - sigmoid(X * theta))) ...
+ + (lambda / (2 * m)) * (sum(theta(2:end) .^ 2));
 
-
-
-
-
-
+thetaZero = (1 / m ) * (X' * (sigmoid(X * theta) - y));
+thetaZero = thetaZero(1);
+grad = (1 / m ) * (X' * (sigmoid(X * theta) - y)) + ((lambda / m) * theta);
+grad(1) = thetaZero;
 
 
 % =============================================================
--- machine-learning-ex3-complete/ex3/oneVsAll.m
+++ machine-learning-ex3-complete/ex3/oneVsAll.m
@@ -14,6 +14,7 @@ n = size(X, 2);
 % You need to return the following variables correctly 
 all_theta = zeros(num_labels, n + 1);
 
+
 % Add ones to the X data matrix
 X = [ones(m, 1) X];
 
@@ -50,14 +51,15 @@ X = [ones(m, 1) X];
 %
 
 
+initial_theta = zeros(n + 1, 1);
+options = optimset('GradObj', 'on', 'MaxIter', 50);
 
+for c = 1:num_labels
 
+	[theta] = fmincg (@(t)(lrCostFunction(t, X, (y == c), lambda)), initial_theta, options);
+	all_theta(c,:) = theta';
 
-
-
-
-
-
+endfor
 
 
 % =========================================================================
